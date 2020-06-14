9928dc1c7e18632874c98e037485ed7bd53ec4ea
sxt <2381689790@qq.com>
2020-06-14 21:39:33 +0800

    homework_03

---
+++ Week_03/77.组合.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=77 lang=python
+#
+# [77] 组合
+#
+
+# @lc code=start
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        nums = list(range(1, k + 1)) + [n + 1]        
+        output, j = [], 0
+        while j < k:
+            output.append(nums[:k])
+            j = 0
+            while j < k and nums[j + 1] == nums[j] + 1:
+                nums[j] = j + 1
+                j += 1
+            nums[j] += 1           
+        return output
+# @lc code=end
+
